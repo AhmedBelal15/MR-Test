5ca6ee99dd9bee5cdecac3f417d580d37b16b5e9
Ahmed Belal <ahmbelal95@gmail.com>
2021-11-12 14:58:59 +0200

    feat: compare passwords util

--- api/utils/password.py
+++ api/utils/password.py
@@ -3,4 +3,7 @@ import bcrypt
 def hash_password(password):
     bstring = password.encode('UTF-8')
     return bcrypt.hashpw(bstring, bcrypt.gensalt())
-     
+
+def compare_passwords(password, hashed_password):
+    bstring = password.encode("UTF-8")
+    return bcrypt.checkpw(bstring, hashed_password)
