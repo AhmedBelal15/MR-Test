8a31d13aa40a66ecb6be3bfb8a34eb0ecd78eaeb
Ahmed Belal <ahmbelal95@gmail.com>
2021-11-12 16:46:37 +0200

    feat: router with protected auth route

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
+import AuthContainer from "./pages/auth/AuthContainer";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+  const currentUser = JSON.parse(localStorage.getItem("user"));
+
   return (
     <div className="App">
       <h1>Marketeer's Research Test</h1>
+      <Router>
+        <Toaster position="bottom-right" reverseOrder={false} />
+        <Route
+          path="/auth"
+          render={() =>
+            !currentUser ? <AuthContainer /> : <Redirect to="/" />
+          }
+        />
+      </Router>
     </div>
   );
 }
